c9678af1158fc232328dcc9c01c3bdfe7b88378e
Douglas James <douglasrcjames@gmail.com>
2018-01-11 12:17:03 -0800

    Removing navbar and header div from header.php
    The ID tag is crucial to each page to have the image behind the navbar, header.php will be smaller now. Will change on each page in a next update

--- header.php
+++ header.php
@@ -17,41 +17,10 @@
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'/>
   <!-- MY CSS FILE-->
   <link rel='stylesheet' href='public/css/design.css' media='all'/>
-  
+
 </head>
-<body>
-	 <!-- Navbar Fixed-->
-	 <nav class='navbar navbar-default'>
-		<div class='container-fluid'>
-		   <div class='col-md-12 navbar-socials'>
-			  <ul class='nav navbar-nav navbar-right'>
-				  <li><span class='call'>Call us at:</span><span class='number'>408-230-6622</span></li>
-				  <li><a class='btn btn-social' href='#'><span class='fa fa-facebook'></span></a></li>
-				  <li><a class='btn btn-social' href='#'><span class='fa fa-linkedin'></span></a></li>
-				  <li><a class='btn btn-social' href='#'><span class='fa fa-google-plus'></span></a></li>
-				  <li>[<strong>TCP</strong> #(33733-B)]</li>
-			  </ul>
-		   </div>
-		   <!-- Brand and toggle get grouped for better mobile display-->
-		   <div class='navbar-header'>
-			  <button class='navbar-toggle collapsed' type='button' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'><span class='sr-only'>Toggle navigation</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button>
-			  <a class='navbar-brand' href='/'>
-				 <div class='brand-logo'><img class='img-responsive' src='./public/img/logo.png'/></div>
-			  </a>
-		   </div>
-		   <!-- Collect the nav links, forms, and other content for toggling-->
-		   <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
-			  <ul class='nav navbar-nav navbar-right'>
-				 <li><a class='active' href='/'>Home</a></li>
-				 <li><a href='/contact.php'>Contact</a></li>
-			  </ul>
-		   </div>
-		</div>
-	 </nav>
 	
 	"
-
-
 
 
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,44 @@
 
 <?php include("header.php") ?>
 
-      <!-- Header Section-->
-  	  <div class='col-md-12 header'>	
+
+	<body>
+	  <!-- Header Section-->
+	  <div class='col-md-12 header'>
+		 <!-- Navbar Fixed-->
+		 <nav class='navbar navbar-default'>
+			<div class='container-fluid'>
+			   <div class='col-md-12 navbar-socials'>
+				  <ul class='nav navbar-nav navbar-right'>
+					  <li><span class='call'>Call us at:</span><span class='number'>408-230-6622</span></li>
+					  <li><a class='btn btn-social' href='#'><span class='fa fa-facebook'></span></a></li>
+					  <li><a class='btn btn-social' href='#'><span class='fa fa-linkedin'></span></a></li>
+					  <li><a class='btn btn-social' href='#'><span class='fa fa-google-plus'></span></a></li>
+					  <li>[<strong>TCP</strong> #(33733-B)]</li>
+				  </ul>
+			   </div>
+			   <!-- Brand and toggle get grouped for better mobile display-->
+			   <div class='navbar-header'>
+				  <button class='navbar-toggle collapsed' type='button' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'><span class='sr-only'>Toggle navigation</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button>
+				  <a class='navbar-brand' href='/'>
+					 <div class='brand-logo'><img class='img-responsive' src='./public/img/logo.png'/></div>
+				  </a>
+			   </div>
+			   <!-- Collect the nav links, forms, and other content for toggling-->
+			   <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+				  <ul class='nav navbar-nav navbar-right'>
+					 <li><a class='active' href='/'>Home</a></li>
+					 <li><a href='/contact.php'>Contact</a></li>
+				  </ul>
+			   </div>
+			</div>
+		 </nav>
+     	
          <div class="col-md-10 col-md-offset-1 header-info text-right">
-           <!-- Script file for Sumo -->
+          
+            <!-- Script file for Sumo -->
 			<script src='//load.sumome.com/' data-sumo-site-id='ef5fbf833ed979bd2e875143df42cbe264d9891eab2ea23215b15ee6709f59fd' async='async'></script>
+           
             <h3>Providing the Best Limousine Service</h3>
             <h2>in the Bay Area!</h2>
             <h4>Call us at:  408-230-6622</h4>
