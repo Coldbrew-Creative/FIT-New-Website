32fa49a03f07a294929b49368e632f488dda2e04
Douglas James <douglasrcjames@gmail.com>
2018-01-10 18:22:16 -0800

    Applied header & footer to index.html
    Will go through each page to make sure it works

---
+++ footer.php
@@ -0,0 +1,63 @@
+<?php
+
+	echo "
+	
+	      <!-- Get Quote Today-->
+      <div class='col-md-12 get-quote text-center'>
+         <div class='col-md-8 col-md-offset-2'>
+            <h2>Get a Quote Today</h2>
+            <!-- Limo Anywhere Plugin--><a href='https://book.mylimobiz.com/v4/bestlimousineservice' data-ores-widget='website' data-ores-alias='bestlimousineservice'>Online Reservations</a><script type='text/javascript' src='https://book.mylimobiz.com/v4/widgets/widget-loader.js'></script>
+         </div>
+      </div>
+      <!-- Footer-->
+      <footer class='col-md-12 text-center'>
+         <div class='col-md-8 col-md-offset-2'><img class='logo' src='public/img/logo.png'/></div>
+         <div class='col-md-12'>
+            <div class='col-md-4'>
+               <div class='row contact inbox text-center'>
+                  <span><img class='img-responsive' src='./public/img/icons/inbox.svg'/></span>
+                  <h3><a href='mailto:reservations@gofitrans.com'>reservations@gofitrans.com</a></h3>
+               </div>
+            </div>
+            <div class='col-md-4'>
+               <div class='row contact location text-center'>
+                  <span><img class='img-responsive' src='./public/img/icons/location.svg'/></span>
+                  <h3>1250 Aviation Ave Suite 135, San Jose CA 95110<br/>(Inside Atlantic Aviation Lobby)</h3>
+               </div>
+            </div>
+            <div class='col-md-4'>
+               <div class='row contact call text-center'>
+                  <span><img class='img-responsive' src='./public/img/icons/call.svg'/></span>
+                  <h3>408-230-6622</h3>
+               </div>
+            </div>
+            <div class='col-md-12'>
+               <div class='nla'><img id='nla-aff' src='public/img/nla.png'/></div>
+               <p class='copyright'>© First Impression Transportation 2017</p>
+            </div>
+         </div>
+      </footer>
+      <div class='modal fade' id='vehicle-popup' tabindex='-1' role='dialog'>
+         <div class='modal-dialog' role='document'>
+            <div class='modal-content'>
+               <div class='col-md-12 modal-body'>
+                  <div class='col-md-12'>
+                     <div class='row vehicle-gallery'></div>
+                  </div>
+                  <div class='col-md-12 vehicle-info'>
+                     <h4 id='modal-vehicle-name'>Share Note</h4>
+                     <p id='modal-vehicle-info'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  </div>
+               </div>
+               <div class='close'><button data-dismiss='modal'><span>×</span></button></div>
+            </div>
+         </div>
+      </div>
+      <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js'></script><script src='public/js/scripts.js'></script>
+   </body>
+</html>
+	
+	"
+
+
+?>
---
+++ header.php
@@ -0,0 +1,67 @@
+<?php
+
+	echo "
+	
+<html>
+<head>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+  <meta charset='UTF-8'/>
+  <!-- Favicon and Title-->
+  <title>FIT — First Impression Transportation</title>
+  <link rel='shortcut icon' href='#'/>
+  <!-- META TAGS-->
+  <meta property='og:title' content='First Impression Transportation'/>
+  <meta property='og:description' content='Providing the Best Limo Service in the Bay!'/>
+  <!-- CSS CDNs-->
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'/>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'/>
+  <!-- MY CSS FILE-->
+  <link rel='stylesheet' href='public/css/design.css' media='all'/>
+   <!-- Script file for Sumo -->
+   <script src='//load.sumome.com/' data-sumo-site-id='ef5fbf833ed979bd2e875143df42cbe264d9891eab2ea23215b15ee6709f59fd' async='async'></script>
+</head>
+<body>
+  <!-- Header Section-->
+  <div class='col-md-12 header'>
+	 <!-- Navbar Fixed-->
+	 <nav class='navbar navbar-default'>
+		<div class='container-fluid'>
+		   <div class='col-md-12 navbar-socials'>
+			  <ul class='nav navbar-nav navbar-right'>
+				  <li><span class='call'>Call us at:</span><span class='number'>408-230-6622</span></li>
+				  <li><a class='btn btn-social' href='#'><span class='fa fa-facebook'></span></a></li>
+				  <li><a class='btn btn-social' href='#'><span class='fa fa-linkedin'></span></a></li>
+				  <li><a class='btn btn-social' href='#'><span class='fa fa-google-plus'></span></a></li>
+				  <li>[<strong>TCP</strong> #(33733-B)]</li>
+			  </ul>
+		   </div>
+		   <!-- Brand and toggle get grouped for better mobile display-->
+		   <div class='navbar-header'>
+			  <button class='navbar-toggle collapsed' type='button' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'><span class='sr-only'>Toggle navigation</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button>
+			  <a class='navbar-brand' href='/'>
+				 <div class='brand-logo'><img class='img-responsive' src='./public/img/logo.png'/></div>
+			  </a>
+		   </div>
+		   <!-- Collect the nav links, forms, and other content for toggling-->
+		   <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+			  <ul class='nav navbar-nav navbar-right'>
+				 <li><a class='active' href='/'>Home</a></li>
+				 <li><a href='/fleet.html'>Fleet</a></li>
+				 <li><a href='/airport.html'>Airport</a></li>
+				 <li><a href='/corporate.html'>Corporate</a></li>
+				 <li><a href='/weddings.html'>Weddings</a></li>
+				 <li><a href='/wine.html'>Wine Tours</a></li>
+				 <li><a href='/quinceanera.html'>Quinceañera</a></li>
+				 <li><a href='/prom.html'>Prom/Events</a></li>
+				 <li><a href='/contact.html'>Contact</a></li>
+			  </ul>
+		   </div>
+		</div>
+	 </nav>
+	
+	"
+
+
+
+
+?>
