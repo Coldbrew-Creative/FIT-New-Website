0d3ee60ed319041a386d85c264484c2975245b98
Mike <mejle12@gmail.com>
2018-03-16 12:48:46 -0700

    Added DocType

--- header.php
+++ header.php
@@ -2,12 +2,38 @@
 
 	echo "
 	
+	<!doctype html>
 <html>
 <head>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
   <meta charset='UTF-8'/>
   <!-- Favicon and Title-->
-  <title>FIT — First Impression Transportation</title>
+	";
+
+	if($title) {
+		echo "<title>" . $title . "</title>";
+	} else {
+		echo "<title>FIT — First Impression Transportation</title>";
+	}
+	
+	if($mdesc) {
+		echo "
+		<meta name=\"description\" content=\"" . $mdesc . "\"/>";
+	} else {
+		echo '
+		<meta name="description" content="Providing the Best Limo Service in the Bay!"/>';
+	}
+	
+	if($mkeywords) {
+		echo "
+		<meta name=\"keywords\" content=\"" . $mkeywords . "\"/>";
+	} else {
+		echo '
+		<meta name="keywords" content=""/>';
+	}
+	
+	echo "
+	
   <link rel='shortcut icon' href='#'/>
   <!-- META TAGS-->
   <meta property='og:title' content='First Impression Transportation'/>
@@ -16,7 +42,17 @@
   <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'/>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'/>
   <!-- MY CSS FILE-->
-  <link rel='stylesheet' href='public/css/design.css' media='all'/>
+  <link rel='stylesheet' href='/public/css/design.css' media='all'/>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-115089868-1\"></script>
+	<script>
+	 window.dataLayer = window.dataLayer || [];
+	 function gtag(){dataLayer.push(arguments);}
+	 gtag('js', new Date());
+
+	 gtag('config', 'UA-115089868-1');
+	</script>
 
 </head>
 	</html>
