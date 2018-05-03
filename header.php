49e24412840f90e05cf781f53b324d4082ecec0f
Mike <mejle12@gmail.com>
2018-05-02 20:57:58 -0700

    fix

--- header.php
+++ header.php
@@ -44,15 +44,6 @@
   <!-- MY CSS FILE-->
   <link rel='stylesheet' href='/public/css/design.css' media='all'/>
 	
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58063008-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-58063008-1');
-</script>
 
 
 </head>
